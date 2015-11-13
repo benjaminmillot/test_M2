d9ed55f3da8788e3b111b6e6ec50901ef629c89d
Benjamin Millot <benjamin.millot@outlook.fr>
2015-11-13 09:49:30 +0100

    Get input file

---
+++ fasta.py
@@ -0,0 +1,9 @@
+__author__ = 'Benjamin Millot'
+__date__ = '13/11/2015'
+
+def fasta_input():
+    file_input = raw_input('Please enter the path of your fasta file:\n')
+    #print '{0}'.format(file_input)
+
+if __name__ == '__main__':
+    fasta_input()
